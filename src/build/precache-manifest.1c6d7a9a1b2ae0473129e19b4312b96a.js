c4982ae35a6cf8650e3ec3e49e7928fe8e5112e8
William Tully <brady.tully@gmail.com>
2020-04-28 16:11:33 -0400

    First commit

---
+++ src/build/precache-manifest.1c6d7a9a1b2ae0473129e19b4312b96a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a9a93cca504d7ff11dab5a677a1674",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f654dc4a0b4f4654331",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "03d6dfdb49e1f0428b3f",
+    "url": "/static/js/2.1fc4d83f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1fc4d83f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f654dc4a0b4f4654331",
+    "url": "/static/js/main.53d64a7e.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
